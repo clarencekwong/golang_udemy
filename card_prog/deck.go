90b61bda0d2a8c72a769e0e807615d5e28db7c4d
github username <clarencekwong7@gmail.com>
2019-05-30 18:15:16 -0400

    lesson 20 done

--- card_prog/deck.go
+++ card_prog/deck.go
@@ -4,6 +4,18 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
+	cardValues := []string{"Aces", "Two", "Three", "Four", "Five", "Six"}
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
